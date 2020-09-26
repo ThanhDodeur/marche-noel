3e5f55ed0e7e23244c8909231b9d3efafa779006
Thanh Dodeur <thanhdodeur@gmail.com>
2020-09-26 15:24:11 +0200

    data processing

--- src/components/Marche/Marche.js
+++ src/components/Marche/Marche.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './Marche.css';
 
 import NavBar from '../NavBar/NavBar.js';
-import PageData from '../PageData/PageData.js';
 import FileInput from '../FileInput/FileInput.js';
 
 class Marche extends React.Component {
@@ -10,7 +9,8 @@ class Marche extends React.Component {
         super();
         this.state = {
             files: [],
-            days: [],
+            days: [], // { customers, missedPayments }
+            suppliers: {}, // { supplierId : { total } }
             resetRequested: false,
         };
     }
@@ -19,7 +19,7 @@ class Marche extends React.Component {
     * @param {blob} blob
     * @return {file}
     */
-    _getFile = async (blob) => {
+    _readFile = async (blob) => {
         const reader = new FileReader();
         reader.readAsText(blob);
         return new Promise((resolve) => {
@@ -27,6 +27,18 @@ class Marche extends React.Component {
                 resolve(reader.result);
             };
         });
+    }
+    _processFiles = async (files) => {
+        const newDays = [];
+        let suppliers = Object.assign({}, this.state.suppliers);
+        for (const file of files) {
+            const page = await this._readFile(file);
+            const result = this._computeFile(page, suppliers);
+            suppliers = result.suppliers;
+            newDays.push(result.day);
+        }
+        const days = this.state.days.concat(newDays);
+        this.setState({ days, suppliers });
     }
     /**
      * Expected structure of page:
@@ -48,79 +60,73 @@ class Marche extends React.Component {
      * @param {String} page open text file
      * @return {Object}
      */
-    _getDay = (page) => {
+    _computeFile = (page, suppliers) => {
         const OFFSET_HEIGHT = 1; // does not include the column titles.
-        const CLIENT_COLS = 3;
-        const FOURNISSEUR_COLS = 3; // should be equal to colNames.length - CLIENT_COLS
         const lines = page.split(/\r\n|\n/);
         for (const i of Array(OFFSET_HEIGHT)) {
             lines.shift();
         }
         const colNames = lines.shift().split(','); // removes and saves column titles.
-        const clients = [];
-        const fournisseurs = [];
+        /*
+        *
+        * customers = { clientId: { supplied , paid } }
+        * supplied = [ {name: 'itemName', 'price': price, 'supplierId': id } ] WHAT IS PAID
+        * paid = [ {name: 'itemName', 'price': price, 'supplierId': id } ]
+        *
+        * suppliers = { supplierId : { total } }
+        *
+        */
+        const customers = {};
         while (lines.length) {
             const currentLine = lines.shift().split(',');
-            // CLIENTS
-            const client = {};
-            for (let i of [...Array(CLIENT_COLS).keys()]) {
-                client[colNames[i]] = currentLine[i];
-            }
-            // FOURNISSEURS
-            const fournisseur = {};
-            for (let i of [...Array(FOURNISSEUR_COLS).keys()]) {
-                const index = i + CLIENT_COLS;
-                fournisseur[colNames[index]] = currentLine[index];
-            }
-            if (currentLine[0]) { // if client id.
-                clients.push(client);
-            }
-            if (currentLine[CLIENT_COLS]) { // if fournisseur id.
-                fournisseurs.push(fournisseur);
-            }
-        }
-        /*
-        * day structure:
-        {
-            clients: [{
-                'numero client': 453,
-                'numero fournisseur': 12,
-                'achat': 55}, {
-            }]
-            days: [{
-                'numero fournisseur': 12,
-                'numero client': 453,
-                'prix': 55}, {
-            }]
-        }
-        */
-        return { clients, fournisseurs };
-    }
-    /**
-    * Handler for File Input onChange.
-    *
-    * @param {file[]} files
-    * @return {void}
-    */
-    onFileInputChange = async (files) => {
-        this.state.files = files;
-        const newDays = [];
-        for (const file of files) {
-            const page = await this._getFile(file);
-            const day = this._getDay(page);
-            newDays.push(day);
-        }
-        const days = this.state.days.concat(newDays);
-        this.setState({ days });
-    }
 
-    resetFiles = () => {
-        this.setState({days: [], pages: []});
-        this.toggleReset();
-    }
+            /* DATA FILL
+            *   paid
+            *   currentLine[0] purchase - customerId
+            *   currentLine[1] purchase - supplierId
+            *   currentLine[2] purchase - item Name
+            *   currentLine[3] purchase - item Price
+            *   supplied
+            *   currentLine[4] payment - supplierId
+            *   currentLine[5] payment - customerId
+            *   currentLine[6] payment - item Name
+            *   currentLine[7] payment - item Price
+            */
 
-    toggleReset = () => {
-        this.setState({ resetRequested: !this.state.resetRequested });
+            if (currentLine[0]) {
+                // creates the customer if it doesn't already exist.
+                customers[currentLine[0]] = customers[currentLine[0]] || {
+                    supplied: [],
+                    paid: [],
+                };
+                // adds a line for what the customer paid
+                customers[currentLine[0]].paid.push({
+                    name: currentLine[2],
+                    price: currentLine[3],
+                    supplierId: currentLine[1],
+                });
+            }
+
+            if (currentLine[4] && currentLine[5]) {
+                customers[currentLine[5]] = customers[currentLine[5]] || {
+                    supplied: [],
+                    paid: [],
+                };
+                // adds a line for what the customer paid
+                customers[currentLine[5]].supplied.push({
+                    name: currentLine[6],
+                    price: currentLine[7],
+                    supplierId: currentLine[4],
+                });
+                suppliers[currentLine[4]] = suppliers[currentLine[4]] || { total: 0 };
+                suppliers[currentLine[4]].total += Number(currentLine[7].replace(',','.'));
+            }
+        }
+        const missedPayments = this._computeMissedPayments(customers);
+        return {day: { customers, missedPayments }, suppliers };
+    }
+    _computeMissedPayments = (customers) => {
+        return [];
     }
     /**
     * Brief description of the function here.
@@ -135,9 +141,56 @@ class Marche extends React.Component {
                 {content: 'Confirm', fa: 'fa-check', className: 'alert', callBack: this.resetFiles},
             ];
         }
-        return [
+        const buttons = [
             {className: 'blue', fa: 'fa-upload', content: (<FileInput label={`Ajouter | jours: ${this.state.days.length}`} className="noselect" value={this.state.files} onChange={this.onFileInputChange} />)},
         ].concat(resetButtons);
+
+        if (this.state.days) {
+            buttons.push({content: 'Calculer', fa: 'fa-plus', className: 'green', callBack: this.computeResults});
+        }
+        return buttons;
+    }
+
+    ////////////////// //////// //////////////////
+    ////////////////// HANDLERS //////////////////
+    ////////////////// //////// //////////////////
+
+    /**
+    * Handler for File Input onChange.
+    *
+    * @param {file[]} files
+    * @return {void}
+    */
+   onFileInputChange = async (files) => {
+        this.state.files = files;
+        await this._processFiles(files);
+    }
+    /**
+    * Brief description of the function here.
+    *
+    * @return {Object[]} [{content, className, fa, callBack}]
+    */
+    computeResults = () => {
+        // add up expenses
+        // add up suppliers sale totals
+        return;
+    }
+    /**
+    * Brief description of the function here.
+    *
+    * @return {Object[]} [{content, className, fa, callBack}]
+    */
+    resetFiles = () => {
+        this.setState({days: [], pages: []});
+        this.toggleReset();
+    }
+    /**
+    * Brief description of the function here.
+    *
+    * @return {Object[]} [{content, className, fa, callBack}]
+    */
+    toggleReset = () => {
+        this.setState({ resetRequested: !this.state.resetRequested });
     }
     /**
      * add encodage popup with button toggle
@@ -152,9 +205,6 @@ class Marche extends React.Component {
             <NavBar
                 buttons={this._getButtons()}
             />
-            {!!this.state.days.length &&
-                <PageData days={this.state.days}/>
-            }
         </div>
     }
 }
