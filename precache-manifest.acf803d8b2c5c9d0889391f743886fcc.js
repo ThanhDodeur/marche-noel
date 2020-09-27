c66c4b632962f84ac18ac47380276c48008e264e
Thanh Dodeur <thanhdodeur@gmail.com>
2020-09-27 19:26:20 +0200

    Updates

---
+++ precache-manifest.acf803d8b2c5c9d0889391f743886fcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e9d0d018a27ecca1158c81cd77b654",
+    "url": "/marche-noel/index.html"
+  },
+  {
+    "revision": "046085ad37cf63c44576",
+    "url": "/marche-noel/static/css/main.c6ddedc9.chunk.css"
+  },
+  {
+    "revision": "a6057e755b5e668cc6d1",
+    "url": "/marche-noel/static/js/2.96c1c249.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marche-noel/static/js/2.96c1c249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046085ad37cf63c44576",
+    "url": "/marche-noel/static/js/main.b5fd5f7f.chunk.js"
+  },
+  {
+    "revision": "306bd831ae8ed914d322",
+    "url": "/marche-noel/static/js/runtime-main.29242d5b.js"
+  },
+  {
+    "revision": "c1c98c92eab72567bc05a1f71a9df81a",
+    "url": "/marche-noel/static/media/snowflake.c1c98c92.svg"
+  }
+]);
